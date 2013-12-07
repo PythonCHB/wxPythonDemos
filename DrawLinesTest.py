3ff843ee5839769aafe191bf001628a04e84118f
Metallicow <edg62702@yahoo.com>
2013-12-06 19:35:05 -0600

    Fix Shebang/Cleanup/Readability

--- DrawLinesTest.py
+++ DrawLinesTest.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.4
+#!/usr/bin/env python
 
 import wx
 import numarray
 from numarray import random_array
-import RandomArray # the Numeric version
+import RandomArray # the Numeric version.
 import time
 
 
@@ -12,31 +12,30 @@ NumPointPoints = 5000
 
 ## Make some random data to draw things with.
 MaxX  = 500
-LinesPoints = random_array.randint(1, MaxX, (NumLinePoints,2) )
-#PointsPoints = random_array.randint(1, MaxX, (NumPointPoints,2) )
-PointsPoints = RandomArray.randint(1, MaxX, (NumPointPoints,2) ) # Numeric
-
+LinesPoints = random_array.randint(1, MaxX, (NumLinePoints, 2))
+#PointsPoints = random_array.randint(1, MaxX, (NumPointPoints, 2))
+PointsPoints = RandomArray.randint(1, MaxX, (NumPointPoints, 2)) # Numeric.
 
 
 class TestFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1, "DrawLines Test",
-                         wx.DefaultPosition,
-                         size=(500,500),
-                         style=wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE)
+                          wx.DefaultPosition,
+                          size=(500,500),
+                          style=wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE)
 
-        ## Set up the MenuBar
+        # Set up the MenuBar.
         MenuBar = wx.MenuBar()
-        
+
         file_menu = wx.Menu()
         ID_EXIT_MENU = wx.NewId()
-        file_menu.Append(ID_EXIT_MENU, "E&xit","Terminate the program")
+        file_menu.Append(ID_EXIT_MENU, "E&xit", "Terminate the program")
         wx.EVT_MENU(self, ID_EXIT_MENU, self.OnQuit)
         MenuBar.Append(file_menu, "&File")
-        
+
         draw_menu = wx.Menu()
         ID_DRAW_MENU = wx.NewId()
-        draw_menu.Append(ID_DRAW_MENU, "&ReDraw","DrawAgain")
+        draw_menu.Append(ID_DRAW_MENU, "&ReDraw", "DrawAgain")
         wx.EVT_MENU(self, ID_DRAW_MENU,self.ReDraw)
         MenuBar.Append(draw_menu, "&Draw")
 
@@ -46,14 +45,14 @@ class TestFrame(wx.Frame):
 
     def OnPaint(self,event):
         dc = wx.PaintDC(self)
-        dc.SetBackground( wx.Brush("White") )
+        dc.SetBackground(wx.Brush("White"))
         dc.Clear()
         self.DrawLines(dc)
         self.DrawPoints(dc)
 
     def ReDraw(self, event = None):
         dc = wx.ClientDC(self)
-        dc.SetBackground( wx.Brush("White") )
+        dc.SetBackground(wx.Brush("White"))
         dc.Clear()
         self.DrawLines(dc)
         self.DrawPoints(dc)
@@ -64,7 +63,7 @@ class TestFrame(wx.Frame):
         start = time.clock()
         #dc.DrawLines(LinesPoints.tolist())
         dc.DrawLines(LinesPoints)
-        print "DrawLines Call took %f seconds"%(time.clock() - start)
+        print("DrawLines Call took %f seconds" %(time.clock() - start))
         dc.EndDrawing()
 
     def DrawPoints(self, dc):
@@ -72,7 +71,7 @@ class TestFrame(wx.Frame):
         dc.SetPen(wx.Pen('Red', 2))
         start = time.clock()
         dc.DrawPointList(PointsPoints)
-        print "DrawPointList Call took %f seconds"%(time.clock() - start)
+        print("DrawPointList Call took %f seconds" %(time.clock() - start))
         dc.EndDrawing()
 
     def OnQuit(self,event):
@@ -89,22 +88,3 @@ class DemoApp(wx.App):
 if __name__ == "__main__":
     app = DemoApp(0)
     app.MainLoop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
